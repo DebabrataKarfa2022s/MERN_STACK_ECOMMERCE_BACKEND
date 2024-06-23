0ded5fe985249c6cc2a2b996f1f3ad29f2b45233
DebabrataKarfa2022s <debabratakarfa2002@gmail.com>
2024-06-23 12:55:06 +0530

    add netlify cors update

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import cors from "cors";
 
 const app=express();
 
-// app.use(cors({
-//     origin:process.env.CROS_ORIGIN,
-//     Credential:true,
-//     optionsSuccessStatus: 200
-// }));
+app.use(cors({
+    origin:'https://6677cb117ffbc44b51e235e6--playful-sundae-fdcd92.netlify.app',
+    Credential:true,
+    optionsSuccessStatus: 200,
+    methods:'GET, HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
