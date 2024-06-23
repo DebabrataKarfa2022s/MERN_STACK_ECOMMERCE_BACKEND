83f8aff471591390bc33a385760d299e735f2533
DebabrataKarfa2022s <debabratakarfa2002@gmail.com>
2024-06-23 16:32:03 +0530

    changes for cors

--- src/app.js
+++ src/app.js
@@ -4,13 +4,26 @@ import cors from "cors";
 
 const app=express();
 
-app.use(cors({
-    origin:'https://6677cb117ffbc44b51e235e6--playful-sundae-fdcd92.netlify.app',
-    Credential:true,
-    // optionsSuccessStatus: 200,
-    methods:'GET, HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders:['Content-Type', 'Authorization'],
-}));
+// app.use(cors({
+//     origin:'https://6677cb117ffbc44b51e235e6--playful-sundae-fdcd92.netlify.app',
+//     Credential:true,
+//     // optionsSuccessStatus: 200,
+//     methods:'GET, HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+
+const corsOptions = {
+    origin: 'https://6677cb117ffbc44b51e235e6--playful-sundae-fdcd92.netlify.app', // Replace with your Netlify URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials
+    allowedHeaders: ['Content-Type', 'Authorization'], // Adjust headers as needed
+  };
+  
+  app.use(cors(corsOptions));
+  
+  // Your routes here
+  app.options('*', cors(corsOptions)); // Preflight requests
+
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
