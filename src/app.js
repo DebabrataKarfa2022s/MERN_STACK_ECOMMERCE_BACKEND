b3ed58f61662bbc72b1a5cd29a07dd9b8cc73b1f
DebabrataKarfa2022s <debabratakarfa2002@gmail.com>
2024-07-24 00:32:05 +0530

    add render origin

--- src/app.js
+++ src/app.js
@@ -12,16 +12,16 @@ const app=express();
 //     allowedHeaders: ['Content-Type', 'Authorization'],
 // }));
 
-// const corsOptions = {
-//     // origin: 'https://dkecommerce.netlify.app/', // Replace with your Netlify URL
-//     origin: '*', // Replace with your Netlify URL
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true, // Allow credentials
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Adjust headers as needed
-//   };
+const corsOptions = {
+    // origin: 'https://dkecommerce.netlify.app/', // Replace with your Netlify URL
+    origin: 'https://mern-stack-ecommerce-frontend-1ynt.onrender.com/', // Replace with your URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials
+    allowedHeaders: ['Content-Type', 'Authorization'], // Adjust headers as needed
+  };
   
-  // app.use(cors(corsOptions));
-  app.use(cors());
+  app.use(cors(corsOptions));
+  // app.use(cors());
   
   // Your routes here
   // app.options('*', cors(corsOptions)); // Preflight requests
